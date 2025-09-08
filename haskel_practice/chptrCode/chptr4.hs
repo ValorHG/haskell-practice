402e5b481b44bba1a0fc0a1a32252e0775e14bbc
Valor <m.i.ilahibaks@students.uu.nl>
2025-09-08 16:03:01 +0200

    chptr 4 +-

--- haskel_practice/chptrCode/chptr4.hs
+++ haskel_practice/chptrCode/chptr4.hs
@@ -1,4 +1,4 @@
---Decid eif an integer is even:
+{---Decid eif an integer is even:
 even :: Integral a => a -> Bool
 even n = n `mod` 2 == 0
 
@@ -8,4 +8,25 @@ splitAt n xs = (take n xs, drop n xs)
 
 --Reciprocation:
 recip :: Fractional a => a -> a
-recip n = 1/n
+recip n = 1/n-}
+
+halve :: [a] -> ([a],[a])
+halve xs = (take (length xs `div` 2) xs, drop ((length xs) `div` 2) xs)
+
+halve' :: [a] -> ([a],[a])
+halve' xs = (take n xs, drop n xs)
+            where n = length xs `div` 2
+
+halve'' :: [a] -> ([a],[a])
+halve'' xs = splitAt ((length xs) `div` 2) xs
+
+third''' :: [a] -> a
+third''' xs = head (tail (tail xs))
+
+third'''' :: [a] -> a
+third'''' xs = if length xs >= 3 then head (drop 2 xs) 
+else head (drop (length xs) xs)
+
+third''''' :: [a] -> a
+third''''' (_:_:x:_) = x
+
